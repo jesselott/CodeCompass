d4ceae0789737cf32afa3bd4b6fb4c1bae92d68c
Jesse Lott <jesse.e.lott@outlook.com>
2017-07-25 07:51:50 -0400

    added formspree form to send emails.

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -13,5 +13,30 @@ $(document).ready(function(){
 	});
 });
 
+$('contact-form').submit(function(e){
+
+	var name = document.getElementById('yourName'),
+	var email = document.getElementById('email'),
+	var message = document.getElementById('msg');
+
+	if (!name.value || !email.value || !message.value)  
+		alertify.error('Please check your entries')
+	
+	else {
+
+		$.ajax({
+			url: "https://formspree.io/jesse.e.lott@outlook.com",
+			method: "POST",
+			data: $(this).serialize(), 
+			datatype: "json"
+
+		});
+
+		e.preventDefault()
+		$(this).get(0).reset()
+		alertify.success('Message Sent')
+	}
+
+});
 
 
