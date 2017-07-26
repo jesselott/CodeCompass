29fd2b08a47bea4664019ab1e098b45f820475f9
Jesse Lott <jesse.e.lott@outlook.com>
2017-07-26 09:14:56 -0400

    deleted past attempts at resoving formspree.

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -13,46 +13,6 @@ $(document).ready(function(){
 	});
 });
 
-// $('#contact-form').submit(function(e){
-
-// 	var name = document.getElementById('yourName').value,
-// 	email = document.getElementById('email').value,
-// 	message = document.getElementById('msg').value;
-
-// 	if (name.value === null || email.value === null || message.value === null)  
-// 		alert('Please check your entries');
-	
-// 	else {
-
-// 		$.ajax({
-// 			url: "https://formspree.io/jesse.e.lott@outlook.com",
-// 			method: "POST",
-// 			data: {$(this).serialize()}, 
-// 			datatype: "json"
-
-// 		});
-
-// 		e.preventDefault();
-// 		$("#contact-form").trigger("reset");
-// 		alert('Message Sent');
-// 	}
-
-// });
-
-// var message = "";
-
-// $("#sendMessage").on("click", function() {
-//     message = $("#contact-form").serialize();
-//     $.ajax({
-//         url: "//formspree.io/jesse.e.lott@outlook.com", 
-//         method: "POST",
-//         data: {message: message},
-//         dataType: "json"
-//     });
-//     alert('I will contact you as soon as I can.  Have a great day!');
-//     return false;
-// });
-
 var $contactForm = $('#contact-form');
 $contactForm.submit(function(e) {
 	e.preventDefault();
